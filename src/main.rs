0c350a41dfabf69426ac76381a135dbb22085e4f
Axel Lord <axel.lord2000@gmail.com>
2023-02-15 16:24:00 +0100

    added file content getter macro

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,59 @@
 use clap::Parser;
-use std::env;
 
-#[cfg(server_dev)]
-use std::path::PathBuf;
+mod server_dev {
+    use clap::Parser;
+    use std::path::PathBuf;
+    use tokio::fs;
 
-#[cfg(server_dev)]
-#[derive(Parser, Debug)]
-struct Cli {
-    server_root: PathBuf,
+    #[derive(Parser, Debug)]
+    pub struct Cli {
+        server_root: PathBuf,
+    }
+
+    impl Cli {
+        #[allow(dead_code)]
+        pub async fn file_content(&self, path: &str) -> String {
+            fs::read_to_string(
+                [self.server_root.clone(), PathBuf::from(path)]
+                    .iter()
+                    .collect::<PathBuf>(),
+            )
+            .await
+            .expect("file requested should exist")
+        }
+    }
+}
+
+mod not_server_dev {
+    use clap::Parser;
+
+    #[derive(Parser, Debug)]
+    pub struct Cli {}
 }
 
 #[cfg(not(server_dev))]
-#[derive(Parser, Debug)]
-struct Cli {}
+use not_server_dev::*;
+#[cfg(server_dev)]
+use server_dev::*;
+
+#[cfg(server_dev)]
+macro_rules! get_file_content {
+    ($file_name:expr, $source_location:expr) => {
+        $source_location.file_content($file_name).await.as_str()
+    };
+}
+
+#[cfg(not(server_dev))]
+macro_rules! get_file_content {
+    ($file_name:expr, $source_location:expr) => {
+        include_str!($file_name)
+    };
+}
 
 #[actix_web::main]
 async fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
-    #[cfg(server_dev)]
-    {
-        println!("Hello, server dev!");
-    }
-
-    #[cfg(not(server_dev))]
-    {
-        println!("Hello, normal dev!");
-    }
-
-    println!(
-        "<{}>",
-        env::var("BOOKMARK_SERVER_DEV").unwrap_or("false".to_string())
-    );
     println!("{cli:#?}");
+    println!("{}", get_file_content!("main.rs", cli));
     Ok(())
 }
