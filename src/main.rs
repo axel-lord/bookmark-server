6e6b774cf21385d79d771db28aa6ae4b76f5ae9e
Axel Lord <axel.lord2000@gmail.com>
2023-02-15 17:21:11 +0100

    changes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 use clap::Parser;
 
 mod server_dev {
@@ -37,23 +38,38 @@ use not_server_dev::*;
 use server_dev::*;
 
 #[cfg(server_dev)]
+#[allow(unused_macros)]
 macro_rules! get_file_content {
     ($file_name:expr, $source_location:expr) => {
-        $source_location.file_content($file_name).await.as_str()
+        $source_location.file_content($file_name).await
     };
 }
 
 #[cfg(not(server_dev))]
+#[allow(unused_macros)]
 macro_rules! get_file_content {
-    ($file_name:expr, $source_location:expr) => {
+    ($file_name:expr, $source_location:expr) => {{
+        let _ = &$source_location;
         include_str!($file_name)
-    };
+    }};
+}
+
+#[get("/")]
+async fn index(data: web::Data<Cli>) -> impl Responder {
+    HttpResponse::Ok().body(get_file_content!("index.html", data))
 }
 
 #[actix_web::main]
 async fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
     println!("{cli:#?}");
-    println!("{}", get_file_content!("main.rs", cli));
+
+    let state = web::Data::new(cli);
+    // println!("{}", get_file_content!("main.rs", cli));
+    HttpServer::new(move || App::new().app_data(state.clone()))
+        .workers(1)
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await?;
     Ok(())
 }
