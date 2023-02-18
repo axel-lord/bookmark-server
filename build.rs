cc166e01343d94d3f4b4edf05d95e7fcc61874cf
Axel Lord <axel.lord2000@gmail.com>
2023-02-18 21:58:55 +0100

    Progress on build.rs

--- build.rs
+++ build.rs
@@ -1,13 +1,50 @@
-use std::env::{self, VarError};
+use quote::quote;
+use std::{
+    env::{self, VarError},
+    fs::File,
+    io::{BufWriter, Write},
+    path::Path,
+};
 
-fn main() {
-    const DEV_VAR_KEY: &str = "BOOKMARK_SERVER_DEV";
+const DEV_VAR_KEY: &str = "BOOKMARK_SERVER_DEV";
+const MAPPINGS: [(&str, &str); 1] = [("index.html", "./web/index.html")];
+
+async fn embed_html(out_file: &Path) {
+    let futures =
+        MAPPINGS.map(|(web_path, file_path)| (web_path, tokio::fs::read_to_string(file_path)));
+
+    let mut writer =
+        BufWriter::new(File::create(out_file).expect("file creation in build dir should succeed"));
+    for (_web_path, future) in futures {
+        let content = future.await.expect("file and content should exist");
+        writeln!(
+            writer,
+            "{}",
+            quote! {
+                fn hello() -> &'static str {
+                    #content;
+                }
+            }
+        )
+        .expect("write should succeed");
+    }
+    writer.flush().expect("flushing of writer should succeed");
+}
+async fn get_html(_out_file: &Path) {}
+
+#[tokio::main]
+async fn main() {
+    let out_dir = env::var_os("OUT_DIR").expect("OUT_DIR enviroment variable should exist");
+    let out_file = Path::new(&out_dir).join("locations.rs");
 
     match env::var(DEV_VAR_KEY) {
         Ok(val) if val.to_lowercase() == "true" => {
-            println!("cargo:rustc-cfg=server_dev")
+            println!("cargo:rustc-cfg=server_dev");
+            get_html(&out_file).await;
         }
-        Err(VarError::NotPresent) | Ok(_) => (),
+        Err(VarError::NotPresent) | Ok(_) => {
+            embed_html(&out_file).await;
+        }
         Err(e) => panic!("issue parsing enviroment variable ${{{DEV_VAR_KEY}}}, {e}"),
     };
 
--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,16 @@
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpServer};
+
 use clap::Parser;
 
-mod server_dev {
-    use clap::Parser;
-    use std::path::PathBuf;
-    use tokio::fs;
-
-    #[derive(Parser, Debug)]
-    pub struct Cli {
-        server_root: PathBuf,
-    }
-
-    impl Cli {
-        #[allow(dead_code)]
-        pub async fn file_content(&self, path: &str) -> String {
-            fs::read_to_string(
-                [self.server_root.clone(), PathBuf::from(path)]
-                    .iter()
-                    .collect::<PathBuf>(),
-            )
-            .await
-            .expect("file requested should exist")
-        }
-    }
-}
-
-mod not_server_dev {
-    use clap::Parser;
-
-    #[derive(Parser, Debug)]
-    pub struct Cli {}
-}
-
-#[cfg(not(server_dev))]
-use not_server_dev::*;
-#[cfg(server_dev)]
-use server_dev::*;
-
-#[cfg(server_dev)]
-#[allow(unused_macros)]
-macro_rules! get_file_content {
-    ($file_name:expr, $source_location:expr) => {
-        $source_location.file_content($file_name).await
-    };
-}
-
-#[cfg(not(server_dev))]
-#[allow(unused_macros)]
-macro_rules! get_file_content {
-    ($file_name:expr, $source_location:expr) => {{
-        let _ = &$source_location;
-        include_str!($file_name)
-    }};
-}
-
-#[get("/")]
-async fn index(data: web::Data<Cli>) -> impl Responder {
-    HttpResponse::Ok().body(get_file_content!("index.html", data))
-}
+#[derive(Parser, Debug)]
+pub struct Cli {}
 
 #[actix_web::main]
 async fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
     println!("{cli:#?}");
+
+    println!(env!("OUT_DIR"));
 
     let state = web::Data::new(cli);
     // println!("{}", get_file_content!("main.rs", cli));
