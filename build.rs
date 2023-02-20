24332e9b6c5d13043d4a82622dae31ff40e6d78d
Axel Lord <axel.lord2000@gmail.com>
2023-02-20 13:22:09 +0100

    updated build script

--- build.rs
+++ build.rs
@@ -1,3 +1,8 @@
+#![warn(clippy::pedantic)]
+
+const DEV_VAR_KEY: &str = "BOOKMARK_SERVER_DEV";
+const DIRECTORY_LISTING: &str = "./serve.txt";
+
 use proc_macro2::{Ident, Span};
 use quote::quote;
 use std::{
@@ -6,29 +11,19 @@ use std::{
     io::{BufWriter, Write},
     path::Path,
 };
+use tap::Pipe;
 
 #[derive(Debug, Clone, Copy)]
-struct Mapping<T = &'static str, U = &'static str, V = &'static str> {
+struct Mapping<T, U, V> {
     function: T,
     web: U,
     file: V,
 }
 
-impl<T, U, V> Mapping<T, U, V> {
-    const fn new(function: T, web: U, file: V) -> Self {
-        Self {
-            function,
-            web,
-            file,
-        }
-    }
-}
+type HomogenousMapping<T> = Mapping<T, T, T>;
 
-const DEV_VAR_KEY: &str = "BOOKMARK_SERVER_DEV";
-const MAPPINGS: [Mapping; 1] = [Mapping::new("index", "index.html", "./web/index.html")];
-
-async fn embed_html(out_file: &Path) {
-    let futures = MAPPINGS.map(
+async fn embed_html(out_file: &Path, mappings: &[HomogenousMapping<&'static str>]) {
+    let futures = mappings.iter().map(
         |Mapping {
              function,
              web,
@@ -60,14 +55,15 @@ async fn embed_html(out_file: &Path) {
     }
     writer.flush().expect("flushing of writer should succeed");
 }
-async fn get_html(out_file: &Path) {
+
+fn get_html(out_file: &Path, mappings: &[HomogenousMapping<&'static str>]) {
     let mut writer =
         BufWriter::new(File::create(out_file).expect("file creation in build dir should succeed"));
     for Mapping {
         function,
         web,
         file,
-    } in MAPPINGS
+    } in mappings
     {
         let function = Ident::new(function, Span::call_site());
         let content = quote! {
@@ -84,19 +80,41 @@ async fn get_html(out_file: &Path) {
 
 #[tokio::main]
 async fn main() {
+    println!("cargo:rerun-if-env-changed={DEV_VAR_KEY}");
+    println!("cargo:rerun-if-changed={DIRECTORY_LISTING}");
+
     let out_dir = env::var_os("OUT_DIR").expect("OUT_DIR enviroment variable should exist");
     let out_file = Path::new(&out_dir).join("locations.rs");
+    let mappings = tokio::fs::read_to_string(DIRECTORY_LISTING)
+        .await
+        .unwrap_or_else(|e| panic!("reading of {DIRECTORY_LISTING} should succeed, {e}"))
+        .pipe(Box::<str>::from)
+        .pipe(Box::leak)
+        .lines()
+        .map(|l| {
+            let mut column_iter = l.split(',').map(str::trim).filter(|l| !l.is_empty());
+            Mapping {
+                function: column_iter
+                    .next()
+                    .expect("every row should have >=1 columns"),
+                web: column_iter
+                    .next()
+                    .expect("every row should have >=2 columns"),
+                file: column_iter
+                    .next()
+                    .expect("every row should have >=3 columns"),
+            }
+        })
+        .collect::<Vec<_>>();
 
     match env::var(DEV_VAR_KEY) {
         Ok(val) if val.to_lowercase() == "true" => {
             println!("cargo:rustc-cfg=server_dev");
-            get_html(&out_file).await;
+            get_html(&out_file, &mappings);
         }
         Err(VarError::NotPresent) | Ok(_) => {
-            embed_html(&out_file).await;
+            embed_html(&out_file, &mappings).await;
         }
         Err(e) => panic!("issue parsing enviroment variable ${{{DEV_VAR_KEY}}}, {e}"),
     };
-
-    println!("cargo:rerun-if-env-changed={DEV_VAR_KEY}")
 }
