947f02997f24bca309e22c18cca18eba847c5c90
Axel Lord <axel.lord2000@gmail.com>
2023-02-28 20:46:48 +0100

    Simplified build script

--- build.rs
+++ build.rs
@@ -1,117 +1,98 @@
 #![warn(clippy::pedantic)]
 
-const DEV_VAR_KEY: &str = "BOOKMARK_SERVER_DEV";
-const DIRECTORY_LISTING: &str = "./serve.txt";
-
-use proc_macro2::{Ident, Span};
+use proc_macro2::{Ident, Span, TokenStream};
 use quote::quote;
 use std::{
-    env::{self, VarError},
+    env,
     fs::{self, File},
-    io::{BufWriter, Write},
+    io::{self, BufWriter, Write},
     path::Path,
 };
 use tap::Pipe;
 
-#[derive(Debug, Clone, Copy)]
-struct Mapping<T, U, V> {
-    function: T,
-    web: U,
-    file: V,
+const DIRECTORY_LISTING: &str = "./serve.txt";
+
+fn leak_string(string: String) -> &'static str {
+    Box::leak(string.into_boxed_str())
 }
 
-type HomogenousMapping<T> = Mapping<T, T, T>;
+fn get_file_path(web_path: &str) -> &'static str {
+    leak_string(format!(
+        "./web{}{web_path}",
+        if web_path.starts_with('/') { "" } else { "/" }
+    ))
+}
 
-fn embed_html(out_file: &Path, mappings: &[HomogenousMapping<&'static str>]) {
-    let mut writer =
-        BufWriter::new(File::create(out_file).expect("file creation in build dir should succeed"));
-    for Mapping {
-        function,
-        web,
-        file,
-    } in mappings
-    {
-        let function = Ident::new(function, Span::call_site());
-        let content = fs::read_to_string(file)
-            .unwrap_or_else(|e| panic!("{file} should exist end be readable, {e}"));
-        let content = quote! {
-            #[get(#web)]
-            async fn #function() -> impl Responder {
-                HttpResponse::Ok().body(#content)
-            }
-        };
-        println!("cargo:rerun-if-changed={file}");
-        writeln!(writer, "{content}").expect("write should succeed");
+fn write_content(
+    mut out: impl Write,
+    mappings: impl Iterator<Item = [&'static str; 3]>,
+    content: impl Fn([&'static str; 3]) -> io::Result<TokenStream>,
+) -> io::Result<()> {
+    for mapping in mappings {
+        writeln!(out, "{}", content(mapping)?)?;
     }
-    writer.flush().expect("flushing of writer should succeed");
+    out.flush()
 }
 
-fn get_html(out_file: &Path, mappings: &[HomogenousMapping<&'static str>]) {
-    let mut writer =
-        BufWriter::new(File::create(out_file).expect("file creation in build dir should succeed"));
-    for Mapping {
-        function,
-        web,
-        file,
-    } in mappings
-    {
-        let function = Ident::new(function, Span::call_site());
-        let content = quote! {
-            #[get(#web)]
-            async fn #function() -> impl Responder {
-                HttpResponse::Ok().body(tokio::fs::read_to_string(#file).await.expect("file read should succeed"))
-            }
+fn get_content([function, web, file]: [&'static str; 3]) -> io::Result<TokenStream> {
+    let function = Ident::new(function, Span::call_site());
+    quote! {
+        #[get(#web)]
+        async fn #function() -> impl Responder {
+            HttpResponse::Ok().body(tokio::fs::read_to_string(#file).await.expect("file read should succeed"))
+        }
+    }.pipe(Ok)
+}
 
-        };
-        writeln!(writer, "{content}").expect("write should succeed");
+fn embed_content([function, web, file]: [&'static str; 3]) -> io::Result<TokenStream> {
+    println!("cargo:rerun-if-changed={file}");
+
+    let function = Ident::new(function, Span::call_site());
+    let content = fs::read_to_string(file)?;
+    quote! {
+        #[get(#web)]
+        async fn #function() -> impl Responder {
+            HttpResponse::Ok().body(#content)
+        }
     }
-    writer.flush().expect("flushing of writer should succeed");
+    .pipe(Ok)
 }
 
-fn main() {
-    println!("cargo:rerun-if-env-changed={DEV_VAR_KEY}");
+fn main() -> io::Result<()> {
     println!("cargo:rerun-if-changed={DIRECTORY_LISTING}");
-
     let out_dir = env::var_os("OUT_DIR").expect("OUT_DIR enviroment variable should exist");
     let out_file = Path::new(&out_dir).join("locations.rs");
-    let mappings = fs::read_to_string(DIRECTORY_LISTING)
-        .unwrap_or_else(|e| panic!("reading of {DIRECTORY_LISTING} should succeed, {e}"))
-        .pipe(Box::<str>::from)
-        .pipe(Box::leak)
+
+    let mappings = fs::read_to_string(DIRECTORY_LISTING)?
+        .pipe(leak_string)
         .lines()
+        .filter(|l| !l.is_empty())
         .map(|l| {
-            let mut column_iter = l.split(',').map(str::trim).filter(|l| !l.is_empty());
-
-            let function = column_iter
-                .next()
-                .expect("every row should have at least 2 columns, not no columns.");
-
-            let web = column_iter
-                .next()
-                .expect("every row should have at least 2 coulmns, not 1 column.");
-
-            let file = column_iter.next().unwrap_or_else(|| {
-                format!("./web{}{web}", if web.starts_with('/') { "" } else { "/" })
-                    .pipe(Box::<str>::from)
-                    .pipe(Box::leak)
+            let mut col_iter = l.splitn(2, ',').map(str::trim).inspect(|l| {
+                assert!(
+                    !l.is_empty(),
+                    "one of the rows in {DIRECTORY_LISTING} contains an empty column"
+                );
             });
 
-            Mapping {
-                function,
-                web,
-                file,
-            }
-        })
-        .collect::<Vec<_>>();
+            let function = col_iter.next().unwrap_or_else(|| {
+                panic!("every row in {DIRECTORY_LISTING} should contain 2 or no columns, 0 found")
+            });
 
-    match env::var(DEV_VAR_KEY) {
-        Ok(val) if val.to_lowercase() == "true" => {
-            println!("cargo:rustc-cfg=server_dev");
-            get_html(&out_file, &mappings);
-        }
-        Err(VarError::NotPresent) | Ok(_) => {
-            embed_html(&out_file, &mappings);
-        }
-        Err(e) => panic!("issue parsing enviroment variable ${{{DEV_VAR_KEY}}}, {e}"),
-    };
+            let web = col_iter.next().unwrap_or_else(|| {
+                panic!("every row in {DIRECTORY_LISTING} should contain 2 or no columns, 1 found")
+            });
+
+            let file = get_file_path(web);
+
+            [function, web, file]
+        });
+
+    let out_file = out_file.pipe(File::create)?.pipe(BufWriter::new);
+    if cfg!(feature = "live") {
+        println!("cargo:warning=compiling in \"live\" configuration, files will be read from disk on request, take care");
+        write_content(out_file, mappings, get_content)
+    } else {
+        write_content(out_file, mappings, embed_content)
+    }
 }
